59b1510e24c28806d51975d875ca243f2e14ae79
ShibaDev <nekostrim@gmail.com>
2023-03-14 10:48:55 -0400

    Error fixed
    Working on a new function

--- Crystal Bird/src/main.cpp
+++ Crystal Bird/src/main.cpp
@@ -1,11 +1,7 @@
 // Some code fragments have been taken from an old repository: https://github.com/ShiibaDev/Fair-Sciencie/blob/master/src/main.cpp
 // They are being optimized and configured for a complex operation.
 
-// Este codigo esta en ingles, para que personas de todo el mundo puedan utilizarlo y configurarlo a su favor.
-
-/* 
-Credits something: ShibaDevelopment(UwU);
-*/
+// Warning, code are corrupted.
 #include <Arduino.h>
 // WiFi Libraries
 #include <SPI.h>
@@ -21,7 +17,6 @@ String pass = "";
 
 // Reset Switch
 String RT = "";
-
 // Functions (Every function made, is below this.)
 
 // Connect to the WiFi ;)
@@ -44,33 +39,6 @@ void Init() {
   Serial.begin(9600);
   Serial.println("Crystal Bird Initialization");
   Serial.print("Connect to wifi? [Y]es [N]o");
-  String wifiConQ;
-  while (Serial.available() > 0) {
-    wifiConQ = Serial.readString();
-  }
-  if (wifiConQ == "Y") {
-    Serial.print("Connection SSID: ");
-    while (Serial.available() > 0) {
-      ssid = Serial.readString();
-    }
-    Serial.println();
-    Serial.print("Connection PASS: ");
-    while (Serial.available() > 0) {
-      pass = Serial.readString();
-    }
-    Serial.println();
-  } else if (wifiConQ == "N") {
-    Serial.println("Installation canceled, please, to restart.\n\n Give input value RT");
-  } else {
-    Serial.println("Invalid input, returning. . .");
-    Init(); return;
-  }
-  // Check the input of RT
-  if (RT == "RT") {
-    Serial.println("Retrying installation procedure");
-    Init();
-    return;
-  }
 }
 
 // Check if the WiFi exist or no, if it exist, then connect.
@@ -118,21 +86,17 @@ void Connection() {
   }
 }
 
-void setup()
-{
-  // put your setup code here, to run once:
+void setup() {
   Init();
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    Serial.println("Already connected");
-  }
-  else
-  {
+  if (ssid == "" && pass == "") {
+    Serial.println("No SSID and Pass gave");
+  } else {
     Connection();
   }
 }
 
-void loop()
-{
+void loop() {
   // put your main code here, to run repeatedly:
 }
+
+// Este codigo esta en ingles, para que personas de todo el mundo puedan utilizarlo y configurarlo a su favor.
